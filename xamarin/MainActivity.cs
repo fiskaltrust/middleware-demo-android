7ef4b0bb8e6caf7ce87878cdc09c05014b535059
Tom Schmiedlechner <tschmiedlechner@outlook.com>
2022-04-08 15:50:30 +0200

    Show content provider access to get logs

--- xamarin/MainActivity.cs
+++ xamarin/MainActivity.cs
@@ -10,17 +10,23 @@ using fiskaltrust.Middleware.Interface.Client.Grpc;
 using fiskaltrust.ifPOS.v1;
 using Newtonsoft.Json;
 using fiskaltrust.Middleware.Interface.Client.Http;
+using System.IO;
+using System.Text;
 
 namespace fiskaltrust.Middleware.Demo
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private const int LOG_REQ_CODE = 1234;
+        
         private const string QUEUE_URL_GRPC = "grpc://localhost:1400";
         private const string QUEUE_URL_REST = "http://localhost:1500/queue";
         private const string CASHBOX_ID = "<your-cashbox-id>";
         private const string ACCESS_TOKEN = "<your-access-token>";
         private const bool SANDBOX = true;
+
+        private TaskCompletionSource<string> _getLogsCompletionSource;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -34,6 +40,7 @@ namespace fiskaltrust.Middleware.Demo
             FindViewById<Button>(Resource.Id.btnSendSignRequest).Click += new EventHandler(async (s, e) => await ButtonSignRequestOnClickAsync());
             FindViewById<Button>(Resource.Id.btnSendStartReceipt).Click += new EventHandler(async (s, e) => await ButtonStartReceiptOnClickAsync());
             FindViewById<Button>(Resource.Id.btnSendZeroReceipt).Click += new EventHandler(async (s, e) => await ButtonZeroReceiptOnClickAsync());
+            FindViewById<Button>(Resource.Id.btnGetLogs).Click += new EventHandler(async (s, e) => await ButtonGetLogsOnClickAsync());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -194,6 +201,53 @@ namespace fiskaltrust.Middleware.Demo
             }
 
             SetButtonsEnabled(true);
+        }
+        
+        public async Task ButtonGetLogsOnClickAsync()
+        {
+            SetButtonsEnabled(false);
+
+            _getLogsCompletionSource = new TaskCompletionSource<string>();
+
+            var componentName = FindViewById<RadioButton>(Resource.Id.radioGrpc).Checked
+                ? new ComponentName("eu.fiskaltrust.androidlauncher.grpc", "eu.fiskaltrust.androidlauncher.grpc.LogContentLinkActivity")
+                : new ComponentName("eu.fiskaltrust.androidlauncher.http", "eu.fiskaltrust.androidlauncher.http.LogContentLinkActivity");
+            
+            var req = new Intent();
+            req.PutExtra("cashboxid", CASHBOX_ID);
+            req.PutExtra("accesstoken", ACCESS_TOKEN);
+            req.SetComponent(componentName);
+
+            StartActivityForResult(req, LOG_REQ_CODE);
+            var log = await _getLogsCompletionSource.Task;
+            var txt = FindViewById<TextView>(Resource.Id.txtLogs);
+
+            if (log != null)
+            {
+                txt.Text = log;
+            }
+            else
+            {
+                txt.Text = "No logs available";
+            }
+
+            SetButtonsEnabled(true);
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (requestCode == LOG_REQ_CODE && resultCode == Result.Ok && data != null)
+            {
+                using var str = ContentResolver.OpenInputStream(Android.Net.Uri.Parse(data.DataString));
+                using var sr = new StreamReader(str, Encoding.UTF8);
+                var log = sr.ReadToEnd();
+                _getLogsCompletionSource?.TrySetResult(log);
+            }
+            else
+            {
+                _getLogsCompletionSource?.TrySetResult(null);
+            }
         }
 
         private async Task<IPOS> GetPOSAsync()
