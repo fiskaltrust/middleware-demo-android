789588516168d0c9ba64bccb34d04b1b9144431c
Paul Volavsek <24523184+volllly@users.noreply.github.com>
2026-09-01 10:12:17 +0200

    improve gradle build

--- java/app/src/main/java/eu/fiskaltrust/middleware/demo/MainActivity.java
+++ java/app/src/main/java/eu/fiskaltrust/middleware/demo/MainActivity.java
@@ -3,7 +3,7 @@ package eu.fiskaltrust.middleware.demo;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
